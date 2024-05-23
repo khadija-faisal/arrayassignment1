670f83834c95cbc3415c22015eb26adb54b69df1
Khadija_Faisal <160174292+khadija-faisal@users.noreply.github.com>
2024-05-23 09:02:10 +0500

    assignment

---
+++ main.ts
@@ -0,0 +1,60 @@
+//    ASSIGNMENT  OF ARRAYS (1)
+
+// //Q no 1
+// //create an array named fruits that contains the following fruits
+// let fruits : string[] = ["apple", "banana", "mango", "orange"]
+
+// // Q no 2
+// // Declare an array named numbers that can contain only number 
+// // elements and initialize it with the values 10, 20, 30, and 40
+// let numbers : number[] = [10, 20, 30, 40,]
+
+// //Q no 3 
+// //Access the third element of the fruits array and assign it to a variable named thirdFruit.
+// let thirdFruit = fruits[2] ;
+
+// //Q no 4
+// //Change the second element of the numbers array to 25
+// numbers[1] = 25 ;
+
+//Q no 5
+//Add the element "grape" to the end of the fruits array using the method.
+// fruits.push("grape");
+
+// //Q no 6
+// //Remove the last element from the fruits array using the 
+// //method and assign it to a variable named lastFruit.
+// let lastFruit = fruits.pop();
+
+//Q no 7
+//Remove the first element from the fruits array using the 
+// //method and assign it to a variable named firstFruit.
+// let firstFruit  = fruits.shift();
+
+//Q no 8
+//Add the element "kiwi" to the beginning of the fruits array using the method.
+// fruits.unshift("kiwi");
+
+// //Q no 9
+// //Remove 2 elements from the fruits array starting from index 1 using the method.
+// fruits.splice(1, 2);
+// //console.log(fruits);
+
+// //Q no 10
+// //Insert the elements "pineapple" and "pear" at index 2 of the fruits array using the method.
+// fruits.splice(2, 0, "pineapple", "pear");
+// //console.log(fruits);
+
+// //Q no 11
+// //Create a new array named citrusFruits that contains the first 
+// //two elements of the fruits array using the method. 
+// let citrusFruits = fruits.slice(0, 2) ;
+// // console.log(citrusFruits);
+
+// //Q no 12
+// //Create a new array named lastTwoFruits that contains the last 
+// //two elements of the fruits array using the  method.
+// let lastTwoFruits = fruits.slice(2, 4);
+// console.log(lastTwoFruits);
+
+
